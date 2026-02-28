6c788c48f05c2f05a9ad3ccf0ada51379d58ca5d
ZH Chen <ziheng_ch@163.com>
2026-02-28 11:11:06 +0100

    update

--- lib/bnn/Geometry/__init__.py
+++ lib/bnn/Geometry/__init__.py
@@ -1,4 +1,2 @@
 from .base import Manifold, ConstantCurvature, Hyperbolic
 from .constantcurvature import Hyperboloid,Klein,Stereographic,Sphere
-from .Grassmannian import GrassmannianGyro
-from .Correlation import CorPolyPoincareBallCholeskyMetric
